9e98575ca0802980e0e40c15ed6572ac68b96e8c
Mayur Jade <jademayur@gmil.com>
2025-02-10 21:53:00 +0530

    .net files

---
+++ EmployeePortal/EmployeePortal/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using EmployeePortal.Models;
+using EmployeePortal.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeRepository _employeeRepository;    
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+            this._employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EmployeesList()
+        {
+            var employees = await _employeeRepository.GetEmployees();
+            return Ok(employees);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SaveEmployee(Employee employee)
+        {
+            await _employeeRepository.SaveEmployee(employee);
+            return Ok(employee);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateEmployee(int id, Employee employee)
+        {
+            await _employeeRepository.UpdateEmployee(id, employee);
+            return Ok(employee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            await _employeeRepository.DeleteEmployee(id);
+            return Ok(id);
+        }   
+    }
+}
---
+++ EmployeePortal/EmployeePortal/Repository/EmployeeRepository.cs
@@ -0,0 +1,55 @@
+﻿using EmployeePortal.Data;
+using EmployeePortal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeePortal.Repository
+{
+    public class EmployeeRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public EmployeeRepository(AppDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+            return await _dbContext.Employees.ToListAsync();
+        }
+
+        public async Task SaveEmployee(Employee employee)
+        {
+            await _dbContext.Employees.AddAsync(employee);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateEmployee(int id, Employee objemp)
+        {
+            var employee = await _dbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                throw new Exception("Employee not found");
+            }
+           
+                employee.Name = objemp.Name;
+                employee.Email = objemp.Email;
+                employee.Mobile = objemp.Mobile;
+                employee.Age = objemp.Age;
+                employee.Salary = objemp.Salary;
+                employee.Status = objemp.Status;
+                await _dbContext.SaveChangesAsync();
+            
+        }
+
+        public async Task DeleteEmployee(int id)
+        {
+            var employee = await _dbContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                throw new Exception("Employee not found");
+            }
+            _dbContext.Employees.Remove(employee);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
